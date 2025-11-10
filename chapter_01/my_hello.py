baa0ddbb20a101a96b6260602e780c777cfa1031
nikolai Yevik <nkvkyk@gmail.com>
2025-11-10 07:28:52 -0600

    test quotes

--- chapter_01/my_hello.py
+++ chapter_01/my_hello.py
@@ -1,3 +1,6 @@
 #print("Hello Python World!", end="\n")
 message = "Hello Python World!"
 print(message)
+print('I told my friend, "Python is my favorite language!"')
+print("The language 'Python' is named after Monty Python, not the snake.")
+print("One of Python's strengths is its diverse and supportive community.")
