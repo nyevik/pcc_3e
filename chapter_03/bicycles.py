9d23d37901443f883397cdc08f70017ce4f04e73
nikolai Yevik <nkvkyk@gmail.com>
2025-11-10 08:15:52 -0600

    print the whole list

--- chapter_03/bicycles.py
+++ chapter_03/bicycles.py
@@ -2,3 +2,7 @@ bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 message = f"My first bicycle was a {bicycles[0].title()}."
 
 print(message)
+print("I have a bicycle brand called " + bicycles[1].title() + ".")
+print("My next bicycle was a " + bicycles[2].title() + ".")
+print("Finally, I got a " + bicycles[3].title() + ".")
+print(bicycles)  # Intentional typo to demonstrate error handling
