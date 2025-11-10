07ba3b4a98c4cf9d18dca913e4eb51ff95bf82d5
nikolai Yevik <nkvkyk@gmail.com>
2025-11-10 08:12:07 -0600

    very basic

--- chapter_01/my_hello.py
+++ chapter_01/my_hello.py
@@ -1,6 +1,18 @@
+import this
+
+
 #print("Hello Python World!", end="\n")
 message = "Hello Python World!"
 print(message)
 print('I told my friend, "Python is my favorite language!"')
 print("The language 'Python' is named after Monty Python, not the snake.")
 print("One of Python's strengths is its diverse and supportive community.")
+name = "ada lovelace"
+print('NAME AS IT IS', name)
+print("CONVERTING TO TITLE", name.title())
+print(name.upper())
+print(name.lower())
+first_name = "ada"
+last_name = "lovelace"
+full_name=f"{first_name} {last_name}"
+print(full_name)
